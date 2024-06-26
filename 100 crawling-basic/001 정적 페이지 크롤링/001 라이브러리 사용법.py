f8490c4b5a506166d96cfc2bc0e34ccc088c5c4e
creedboy <polozh@naver.com>
2024-06-26 13:35:16 +0900

    크롤링 수정

--- 100 crawling-basic/001 정적 페이지 크롤링/001 라이브러리 사용법.py
+++ 100 crawling-basic/001 정적 페이지 크롤링/001 라이브러리 사용법.py
@@ -1,9 +1,10 @@
 import requests
-import urllib3
+# import urllib3
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-response = requests.get("https://startcoding.pythonanywhere.com/basic", verify=False)
+# response = requests.get("https://startcoding.pythonanywhere.com/basic", verify=False)
+response = requests.get("https://startcoding.pythonanywhere.com/basic")
 # print(response.status_code)
 
 print(response.text)
