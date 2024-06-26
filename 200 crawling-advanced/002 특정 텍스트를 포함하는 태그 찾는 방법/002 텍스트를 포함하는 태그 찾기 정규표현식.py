3f14a8250e37ae712e7c318fb26dd53a5cfc755d
creedboy <polozhzh@gmail.com>
2024-06-26 22:26:06 +0900

    텍스트를 포함하는 태그 찾기

--- 200 crawling-advanced/002 특정 텍스트를 포함하는 태그 찾는 방법/002 텍스트를 포함하는 태그 찾기 정규표현식.py
+++ 200 crawling-advanced/002 특정 텍스트를 포함하는 태그 찾는 방법/002 텍스트를 포함하는 태그 찾기 정규표현식.py
@@ -57,3 +57,56 @@ find = soup.find_all('a', string=re.compile('KR$'))
 print("===========")
 print(find)
 
+# 텍스트를 포함하는 태그 하나 찾기
+print("===========")
+print(soup.find('a', string=re.compile('그레이')))
+
+# 텍스트를 포함하는 태그 여러개 찾기
+print("===========")
+print(soup.find_all('a', string=re.compile('그레이')))
+
+# 텍스트로 시작하는 태그 하나 찾기
+print("===========")
+print(soup.find('a', string=re.compile('^삼성전자')))
+
+# 텍스트로 시작하는 태그 여러개 찾기
+print("===========")
+print(soup.find_all('a', string=re.compile('^삼성전자')))
+
+# 텍스트로 끝나는 태그 하나 찾기
+print("===========")
+print(soup.find('a', string=re.compile('KR$')))
+
+# 텍스트로 끝나는 태그 여러개 찾기
+print("===========")
+print(soup.find_all('a', string=re.compile('KR$')))
+
+# 요소 내부에 다른 태그가 있을 경우, 문자열 매칭이 제대로 동작하지 않음
+print(soup.find_all('h4', string=re.compile('원')))  # 10개가 나와야 하지만 8개만 나옴
+
+# (심화) 요소 내부에 다른 태그가 있을 경우
+result = []
+tags = soup.select('.product-price')
+print("===========")
+print(tags)
+
+for tag in tags:
+    # print(tag.text)
+    if '원' in tag.text:
+        result.append(tag)
+
+print("=========== result")
+print(result)
+
+print("=========== contents")
+for tag in tags:
+    # print(tag.contents)
+    print(tag.contents[0])
+
+result2 = []
+for tag in tags:
+    if '원' in tag.contents[0]:
+        result2.append(tag.contents[0].strip())
+
+print("=========== result2")
+print(result2)
