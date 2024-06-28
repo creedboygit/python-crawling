12c3074cd68b782f131eda25bdb9c411d7573ecd
creedboy <polozh@naver.com>
2024-06-28 13:48:25 +0900

    icecream 적용

--- 200 crawling-advanced/002 특정 텍스트를 포함하는 태그 찾는 방법/001 특정 텍스트로 태그 찾는 법.py
+++ 200 crawling-advanced/002 특정 텍스트를 포함하는 태그 찾는 방법/001 특정 텍스트로 태그 찾는 법.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from icecream import ic
 
 response = requests.get("https://startcoding.pythonanywhere.com/basic")
 html = response.text
@@ -9,10 +10,13 @@ soup = BeautifulSoup(html, 'html.parser')
 텍스트와 똑같은 태그 하나 찾기
 '''
 find = soup.find('p', string='노트북')
-print(find)
+# print(find)
+# ic.ic(find)
+ic(find)
 
 '''
 텍스트와 똑같은 태그 여러개 찾기
 '''
 find = soup.find_all('p', string='노트북')
-print(find)
+# print(find)
+ic(find)
