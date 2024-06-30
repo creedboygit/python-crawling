1748ed8daac5d39475901da1b48ad53ed0546a9c
creedboy <polozhzh@gmail.com>
2024-06-30 21:52:28 +0900

    열 너비 설정 추가

--- 200 crawling-advanced/004 네이버 지식인 크롤링/004 엑셀 저장.py
+++ 200 crawling-advanced/004 네이버 지식인 크롤링/004 엑셀 저장.py
@@ -6,10 +6,17 @@ import requests
 from bs4 import BeautifulSoup
 from icecream import ic
 import pandas as pd
+# from openpyxl.reader.excel import load_workbook
+from openpyxl import load_workbook
 
 # input_page = 3
 
-input_page = int(input("몇 페이지까지 수집하시겠습니까? >>> "))
+# input_page = int(input("몇 페이지까지 수집하시겠습니까? >>> "))
+input_text = input("몇 페이지까지 수집하시겠습니까? >>> ")
+if input_text == "":
+    input_page = 1
+else:
+    input_page = int(input_text)
 
 data = []
 # for page in range(1, input_page):
@@ -52,7 +59,7 @@ for page in range(1, input_page + 1):
         ic.enable()
         ic(title, link, date, category, reply_count)
 
-        data.append([title, link, date, category, reply_count])
+        data.append([title, link, date, category, int(reply_count)])
 
         i += 1
 
@@ -62,7 +69,29 @@ for page in range(1, input_page + 1):
 df = pd.DataFrame(data, columns=['제목', '링크', '날짜', '카테고리', '답변수'])
 
 # 엑셀 저장
-# df.to_excel("naver_finance_crawling.xlsx")
+excel_path = "naver_finance_crawling.xlsx"
+# df.to_excel(excel_path, engine="openpyxl", index=False, freeze_panes=(1, 1))
+df.to_excel(excel_path, engine="openpyxl", index=False)
+
+# 엑셀 파일 열기
+wb = load_workbook(excel_path)
+ws = wb.active
+
+# 열 너비 설정
+ws.column_dimensions['a'].width = 70
+ws.column_dimensions['b'].width = 100
+ws.column_dimensions['c'].width = 30
+ws.column_dimensions['d'].width = 30
+ws.column_dimensions['e'].width = 10
+
+# 각 열의 너비를 200으로 설정
+# for column_cells in ws.columns:
+#     length = 50
+#     col_letter = column_cells[0].column_letter
+#     ws.column_dimensions[col_letter].width = length
+
+# 엑셀 파일 저장
+wb.save(excel_path)
 
 # 한글 깨짐 방지, index 번호 컬럼 미노출
-df.to_csv("naver_kin_crawling.csv", index=False, encoding="utf-8-sig")
+# df.to_csv("naver_kin_crawling.csv", index=False, encoding="utf-8-sig")
