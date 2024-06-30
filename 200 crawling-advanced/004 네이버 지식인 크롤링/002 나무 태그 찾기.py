101dfe30f98162e63e5fbb3fd127a6021f9f7f2d
creedboy <polozhzh@gmail.com>
2024-06-30 19:12:52 +0900

    나무 태그 찾기

---
+++ 200 crawling-advanced/004 네이버 지식인 크롤링/002 나무 태그 찾기.py
@@ -0,0 +1,47 @@
+# 2단계
+# 나무 태그 찾기
+import time
+
+import requests
+from bs4 import BeautifulSoup
+from icecream import ic
+import pandas as pd
+
+response = requests.get("https://kin.naver.com/search/list.naver?query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90")
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+title = soup.select_one("._nclicks\\:kin\\.txt").text.strip()
+ic(title)
+
+title = soup.select_one("._searchListTitleAnchor").text.strip()
+ic(title)
+
+title_select = soup.select_one("._nclicks\\:kin\\.txt._searchListTitleAnchor")
+title = title_select.text.strip()
+ic(title)
+
+link = title_select.attrs['href']
+ic(link)
+
+date = soup.select_one(".txt_inline").text
+ic(date)
+
+# data_block = soup.select_one(".txt_block").contents
+# ic(data_block)
+#
+# ic(data_block[1].text)
+# ic(data_block[3].text)
+#
+# category = data_block[1].text + " > " + data_block[3].text
+# ic(category)
+#
+# reply_count = data_block[7].text.split(" ")[1]
+# ic(reply_count)
+
+category = soup.select_one(".txt_block > a:nth-of-type(2)").text
+ic(category)
+
+# reply_count = soup.select_one(".txt_block > span:nth-of-type(2)").text.split(' ')[1]
+reply_count = soup.select_one(".txt_block > span:nth-of-type(2)").text.split("답변수")[1].strip()
+ic(reply_count)
