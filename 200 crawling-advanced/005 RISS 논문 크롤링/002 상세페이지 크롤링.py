205aa81f76e4a67e9ba805abf8d666134d3eebea
creedboy <polozhzh@gmail.com>
2024-07-01 23:26:27 +0900

    상세 페이지 요청 함수

--- 200 crawling-advanced/005 RISS 논문 크롤링/002 상세페이지 크롤링.py
+++ 200 crawling-advanced/005 RISS 논문 크롤링/002 상세페이지 크롤링.py
@@ -61,3 +61,14 @@ for article in articles[:2]:
     ''' 상세 페이지 요청 끝 '''
 
     ic("\n\n")
+
+def get_detail(link):
+    header = {
+        "referer": link.encode('utf-8')
+    }
+
+    detail_response = requests.get(link, headers=header)
+    detail_html = detail_response.text
+    detail_soup = BeautifulSoup(detail_html, 'html.parser')
+
+    return detail_soup
