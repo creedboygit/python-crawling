bca3754103418f405f14fdf9e65c0a1b18a670cc
creedboy <polozhzh@gmail.com>
2024-07-06 20:01:11 +0900

    로그인 이벤트 테스트

--- 200 crawling-advanced/013 PySide6 GUI 프로그램/001 pyside6 기초 사용법.py
+++ 200 crawling-advanced/013 PySide6 GUI 프로그램/001 pyside6 기초 사용법.py
@@ -8,6 +8,12 @@ class MainWindow(QWidget, Ui_Dialog):
         super().__init__()
         self.setupUi(self)
 
+        # self.객체이름.clicked.connect(self.실행할메서드이름)
+        self.login_btn.clicked.connect(self.login)
+
+    def login(self):
+        print(f"아이디: {self.id.text()} / 비밀번호: {self.pw.text()}")
+
 
 app = QApplication()
 
