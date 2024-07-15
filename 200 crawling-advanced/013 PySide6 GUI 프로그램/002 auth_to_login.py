ba95dfef6c12908a4e57f81314af15109a69b913
creedboy <polozhzh@gmail.com>
2024-07-15 21:38:07 +0900

    웹서버의 인증키와 비교하여 인증 성공 여부 판단

--- 200 crawling-advanced/013 PySide6 GUI 프로그램/002 auth_to_login.py
+++ 200 crawling-advanced/013 PySide6 GUI 프로그램/002 auth_to_login.py
@@ -1,7 +1,10 @@
 import sys
+
+import requests
 from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
 from ui_auth import Ui_Form
 from ui_login import Ui_Dialog
+from icecream import ic
 
 
 class AuthWindow(QWidget, Ui_Form):
@@ -9,16 +12,27 @@ class AuthWindow(QWidget, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.login_btn.clicked.connect(self.authenticate)
-        self.lineEdit.returnPressed.connect(self.login_btn.click)
+        self.auth_key_input.returnPressed.connect(self.login_btn.click)
         self.auth_key = "1234"  # 실제 사용 시 보안을 위해 이 부분을 적절히 수정해야 합니다
 
     def authenticate(self):
-        if self.lineEdit.text() == self.auth_key:
+        scrom_auth_result = self.scrom_auth(self.auth_key_input.text())
+        # if self.lineEdit.text() == self.auth_key:
+        if (scrom_auth_result['status'] == 'success'):
             self.login_window = LoginWindow()
             self.login_window.show()
             self.close()
         else:
             QMessageBox.warning(self, "인증 실패", "잘못된 인증키입니다.")
+            self.auth_key_input.clear()
+            self.auth_key_input.setFocus()
+
+    def scrom_auth(self, auth_key):
+        response = requests.get(f"http://scrom.co.kr/test/20240712_authapi_test/test.html?auth_key={auth_key}")
+        json = response.json()
+        ic(json)
+        return json
+
 
 
 class LoginWindow(QWidget, Ui_Dialog):
