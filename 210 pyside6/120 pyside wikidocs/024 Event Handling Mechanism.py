df60a3dad313ca3c2e7efe1b7ce83103b6a22b15
creedboy <polozhzh@gmail.com>
2024-07-20 17:29:03 +0900

    Event Handling Mechanism

---
+++ 210 pyside6/120 pyside wikidocs/024 Event Handling Mechanism.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt6.QtWidgets import (QApplication,
+                             QMainWindow, QLabel)
+from PyQt6.QtCore import Qt
+
+
+class MW(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle("Event Handling Ex")
+        label = QLabel(
+            """<p>Press the <b>ESC</b> key
+            to quit this program.</p>""")
+        self.setCentralWidget(label)
+        self.show()
+
+    def keyPressEvent(self, event):
+        """Reimplement the key press event to close the
+        window."""
+        if event.key() == Qt.Key.Key_Escape:
+            print("ESC key pressed!")
+            self.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MW()
+    sys.exit(app.exec())
