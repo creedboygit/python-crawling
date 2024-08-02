248acd3c4f389c815fa04b5bc68f510ae29e27a8
creedboy <polozh@naver.com>
2024-08-02 14:46:40 +0900

    타이어 샘플

--- 700 inquiry/030 tire/tire_crawling.py
+++ 700 inquiry/030 tire/tire_crawling.py
@@ -6,18 +6,20 @@ header = dict()
 
 header["Host"] = "widget.driverreviews.com"
 header["Connection"] = "keep-alive"
-header["sec-ch-ua"] =  '"Whale";v="3", "Not-A.Brand";v="8", "Chromium";v="126"'
+header["Content-Length"] = "148"
+header["sec-ch-ua"] = "\"Whale\";v=\"3\", \"Not-A.Brand\";v=\"8\", \"Chromium\";v=\"126\""
 header["content-type"] = "application/json"
 header["sec-ch-ua-mobile"] = "?0"
 header["authorization"] = "742f09e77142471324bed5f1fcdb384121e070b2"
 header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Whale/3.27.254.15 Safari/537.36"
-header["sec-ch-ua-platform"] = "macOS"
+header["sec-ch-ua-platform"] = "\"macOS\""
 header["Accept"] = "*/*"
 header["Origin"] = "https://www.allopneus.com"
 header["Sec-Fetch-Site"] = "cross-site"
 header["Sec-Fetch-Mode"] = "cors"
 header["Sec-Fetch-Dest"] = "empty"
 header["Referer"] = "https://www.allopneus.com/"
+header["Accept-Encoding"] = "gzip, deflate, br, zstd"
 header["Accept-Language"] = "ko-KR,ko;q=0.9"
 
 data = '''
